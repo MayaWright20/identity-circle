03a62f2d15f1fc46111a2aaa9a111c8ea04d3b19
mayagwright20 <mayagwright20@hotmail.com>
2025-11-10 18:29:39 +0000

    Refactor backend user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,20 +3,6 @@ import { User } from "../models/user.js";
 import ErrorHandler from "../utils/error.js";
 import { cookieOptions, sendToken } from "../utils/feature.js";
 
-export const signUp = asyncError(async (req, res, next) => {
-  const { name, email, password, username, phone } = req.body;
-
-  let user = await User.findOne({
-    $or: [{ email: email || username }, { username: email || username }],
-  });
-
-  if (user) return next(new ErrorHandler("User already exists", 400));
-
-  user = await User.create({ name, email, password, username, phone });
-
-  sendToken(user, res, "Welcome!", 201);
-});
-
 export const login = asyncError(async (req, res, next) => {
   const { email, password, username } = req.body;
 
@@ -35,4 +21,53 @@ export const login = asyncError(async (req, res, next) => {
   }
 
   sendToken(user, res, `Hey ${user.name}`, 200);
+});
+
+export const logout = asyncError(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("token", "", {
+      ...cookieOptions,
+      expires: new Date(Date.now()),
+    })
+    .json({
+      success: true,
+      message: "Logged out successfully ",
+    });
+});
+
+export const signUp = asyncError(async (req, res, next) => {
+  const { name, email, password, username, phone } = req.body;
+
+  let user = await User.findOne({
+    $or: [{ email: email || username }, { username: email || username }],
+  });
+
+  if (user) return next(new ErrorHandler("User already exists", 400));
+
+  user = await User.create({ name, email, password, username, phone });
+
+  sendToken(user, res, "Welcome!", 201);
+});
+
+export const getMyProfile = asyncError(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+export const deleteMyProfile = asyncError(async (req, res, next) => {
+  const user = await User.findByIdAndDelete(req.user._id);
+
+  if (!user) {
+    return next(new ErrorHandler("User not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Profile deleted successfully",
+  });
 });
